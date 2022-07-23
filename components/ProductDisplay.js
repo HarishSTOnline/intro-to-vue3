3835bd2dfc36b2ccf1cb7b1d551e9eab623c86a3
Harish S <HarishSTProjects@gmail.com>
2022-07-24 00:08:30 +0530

    L10 - Start

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -1,13 +1,13 @@
-app.component("product-display", {
-    props: {
-        premium: {
-            type: Boolean,
-            required: true
-        }
-    },
-    template:
-        /*html*/
-        `<div class="product-display">
+app.component('product-display', {
+  props: {
+    premium: {
+      type: Boolean,
+      required: true
+    }
+  },
+  template: 
+  /*html*/
+  `<div class="product-display">
     <div class="product-container">
       <div class="product-image">
         <img v-bind:src="image">
@@ -18,9 +18,11 @@ app.component("product-display", {
         <p v-if="inStock">In Stock</p>
         <p v-else>Out of Stock</p>
 
-        <p>{{ shipping }}</p>
-    
-        <product-details :details="details"></product-details>
+        <p>Shipping: {{ shipping }}</p>
+
+        <ul>
+          <li v-for="detail in details">{{ detail }}</li>
+        </ul>
 
         <div 
           v-for="(variant, index) in variants" 
@@ -30,53 +32,51 @@ app.component("product-display", {
           :style="{ backgroundColor: variant.color }">
         </div>
         
-        <button class="button" :class="{ disabledButton: !inStock }" :disabled="!inStock" v-on:click="addToCart">Add to Cart</button>
+        <button 
+          class="button" 
+          :class="{ disabledButton: !inStock }" 
+          :disabled="!inStock" 
+          v-on:click="addToCart">
+          Add to Cart
+        </button>
       </div>
     </div>
   </div>`,
-    data() {
-        return {
-            cart: 0,
-            product: "Socks",
-            brand: "Vue Mastery",
-            selectedVariant: 0,
-            details: ["50% cotton", "30% wool", "20% polyester"],
-            variants: [
-                {
-                    id: 2234,
-                    color: "green",
-                    image: "./assets/images/socks_green.jpg",
-                    quantity: 50,
-                },
-                {
-                    id: 2235,
-                    color: "blue",
-                    image: "./assets/images/socks_blue.jpg",
-                    quantity: 0,
-                },
-            ],
-        };
-    },
-    methods: {
-        addToCart() {
-            this.cart += 1;
-        },
-        updateVariant(index) {
-            this.selectedVariant = index;
-        },
-    },
-    computed: {
-        title() {
-            return this.brand + " " + this.product;
-        },
-        image() {
-            return this.variants[this.selectedVariant].image;
-        },
-        inStock() {
-            return this.variants[this.selectedVariant].quantity;
-        },
-        shipping() {
-            return this.premium ? 'Free' : '₹299'
+  data() {
+    return {
+        product: 'Socks',
+        brand: 'Vue Mastery',
+        selectedVariant: 0,
+        details: ['50% cotton', '30% wool', '20% polyester'],
+        variants: [
+          { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+          { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
+        ]
+    }
+  },
+  methods: {
+      addToCart() {
+          this.cart += 1
+      },
+      updateVariant(index) {
+          this.selectedVariant = index
+      }
+  },
+  computed: {
+      title() {
+          return this.brand + ' ' + this.product
+      },
+      image() {
+          return this.variants[this.selectedVariant].image
+      },
+      inStock() {
+          return this.variants[this.selectedVariant].quantity
+      },
+      shipping() {
+        if (this.premium) {
+          return 'Free'
         }
-    },
-});
+        return 2.99
+      }
+  }
+})
