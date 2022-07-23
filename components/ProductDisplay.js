1aef1ebd0d516c66583dc23ed008e59362bb9553
Harish S <HarishSTProjects@gmail.com>
2022-07-24 00:21:46 +0530

    L11 - Start

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -19,7 +19,6 @@ app.component('product-display', {
         <p v-else>Out of Stock</p>
 
         <p>Shipping: {{ shipping }}</p>
-
         <ul>
           <li v-for="detail in details">{{ detail }}</li>
         </ul>
@@ -40,14 +39,6 @@ app.component('product-display', {
           Add to Cart
         </button>
 
-        <button 
-          class="button" 
-          :class="{ disabledButton: !inStock }" 
-          :disabled="!inStock" 
-          v-on:click="removeFromCart">
-          Remove from Cart
-        </button>
-
       </div>
     </div>
   </div>`,
@@ -66,9 +57,6 @@ app.component('product-display', {
   methods: {
       addToCart() {
           this.$emit('add-to-cart', this.variants[this.selectedVariant].id)
-      },
-      removeFromCart() {
-        this.$emit('remove-from-cart', this.variants[this.selectedVariant].id)
       },
       updateVariant(index) {
           this.selectedVariant = index
